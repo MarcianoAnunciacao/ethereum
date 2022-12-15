699b287ffb9b291f665e60552a612f9f9ddd7a4e
MarcianoAnunciacao <marcianoanunciacao@gmail.com>
2022-12-14 21:17:45 -0300

    Added deploy file to connect with Infura and Goerlic

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,28 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider'); 
+const Web3 = require('web3');
+const { interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    '',
+    'https://goerli.infura.io/v3/edc8a92e9bb0490aaac8ab218641ca55'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    try {
+        const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+    console.log('Contract deployed to', result.options.address);    
+    } catch (error) {
+        console.log('Error -> ', error);    
+    }
+    
+    provider.engine.stop();
+};
+deploy();
