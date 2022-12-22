13c9102acc68e231c44bda3fee35f818720e7748
MarcianoAnunciacao <marcianoanunciacao@gmail.com>
2022-12-22 20:53:26 -0300

    Added enter method to bet

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -2,8 +2,15 @@ pragma solidity ^0.4.17;
 
 contract Lottery{
     address public manager;
+    address[] public players;
 
-    function Lottery() public {
+    constructor() public {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 1 ether, "1 Ether is required to Bet.");
+        players.push(msg.sender);
+
     }
 }
