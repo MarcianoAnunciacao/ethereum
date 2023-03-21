38422e6c8f767155f3e63f3d4b8364bfa5011899
MarcianoAnunciacao <marcianoanunciacao@gmail.com>
2023-03-21 16:59:18 -0300

    Added function to aprrove a request

--- kickstarter/contracts/Campaign.sol
+++ kickstarter/contracts/Campaign.sol
@@ -1,17 +1,23 @@
 pragma solidity ^0.8.18;
 // SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+
 contract Campaign {
     struct Request {
         string description;
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint minimunContribution;
-    address [] public approvers;
+    mapping(address => bool) public approvers;
+    uint numRequests;
 
     modifier restricted(){
         require(msg.sender == manager);
@@ -26,17 +32,28 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimunContribution);
 
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
     }
 
-    function createRequest(string memory _description, uint _value, address _recipient) public restricted{
-        requests.push( Request({
-            description: _description,
-            value: _value,
-            recipient: _recipient,
-            complete: false
-        }));
+    function createRequest(string memory _description, uint _value, address _recipient) public payable restricted{
+        console.log("Enter");
+        require(approvers[msg.sender]);
+        console.log("Pass");
+        Request storage request = requests[numRequests++];
+        request.description = _description;
+        request.value = _value;
+        request.recipient = _recipient;
+        request.complete = false;
+        request.approvalCount = 0;        
     }
 
+    function aprroveRequest(uint index) public {
+        Request storage request = requests[index];
 
+        require(approvers[msg.sender]);
+        require(request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;        
+    }
 }
